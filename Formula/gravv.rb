62bccef16dfe8a010269cf4e9aecb39f0a5ce8b9
Oluwatobi Bello <bellotobiloba01@gmail.com>
2026-03-24 18:25:27 +0100

    Update gravv to v0.1.6
    - Add AI integration documentation and workflow code generator
    - New commands: docs workflows, docs errors, docs states
    - New command: docs client workflow <id> --lang <lang>
    - Generate Python/TypeScript/Go workflow implementations

--- Formula/gravv.rb
+++ Formula/gravv.rb
@@ -6,28 +6,28 @@
 class Gravv < Formula
   desc "CLI/TUI tool for the Gravv financial payments platform"
   homepage "https://github.com/GravityFinance/gravv-cli-releases"
-  version "0.1.5"
+  version "0.1.6"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.5/gravv_0.1.5_darwin_amd64.tar.gz"
-      sha256 "1aed932853cdc592a16a006d0d1be19c3c593e0516615082ab0c187e85c68cc3"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.6/gravv_0.1.6_darwin_amd64.tar.gz"
+      sha256 "09ce00eefaee100e1c1c7db52f212f45a61a93e3213b0af6902389abacd78d70"
     end
     on_arm do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.5/gravv_0.1.5_darwin_arm64.tar.gz"
-      sha256 "5b72d77a057a836496430eb6a5dcde87299abb582b2a6083b06b7ba0df771b22"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.6/gravv_0.1.6_darwin_arm64.tar.gz"
+      sha256 "4251ee12a1f25aca3af5d7814dec9943d4af367f144f88f1cb71e1881977ed6e"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.5/gravv_0.1.5_linux_amd64.tar.gz"
-      sha256 "d05975a9d91a96833e717b45210ad0029f3f0efc650a2554c78aa04c62cb5d8d"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.6/gravv_0.1.6_linux_amd64.tar.gz"
+      sha256 "f035387eadb158b8be8f1032f0d13384ddb309e6f8d7cf4551a131e0eedc70bd"
     end
     on_arm do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.5/gravv_0.1.5_linux_arm64.tar.gz"
-      sha256 "136851a911c0a8a31b9c3e7437cd0b2eeeec989b1e2d9db09257289010e00e76"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.6/gravv_0.1.6_linux_arm64.tar.gz"
+      sha256 "6db0504b80c43cbb1d2cb4ae6d40299ac8a68e64e3343a9f570d1cc0558e34ff"
     end
   end
 
