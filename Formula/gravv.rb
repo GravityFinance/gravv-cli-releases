728e92b25958cd7795c08be709d8e7bc4a573d1c
Oluwatobi Bello <bellotobiloba01@gmail.com>
2026-03-25 13:34:40 +0100

    fix: update SHA256 hashes for v0.1.9 (stable)

--- Formula/gravv.rb
+++ Formula/gravv.rb
@@ -12,22 +12,22 @@ class Gravv < Formula
   on_macos do
     on_intel do
       url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.9/gravv_0.1.9_darwin_amd64.tar.gz"
-      sha256 "d3da48b92d1641ee2ce7fd24e655b2d319ecab74b4d030646067e11f242411de"
+      sha256 "2841f990333197b7ed56a4f0b4efd974f4fa55fd9233237fa50feb01564836bd"
     end
     on_arm do
       url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.9/gravv_0.1.9_darwin_arm64.tar.gz"
-      sha256 "6700480a3a8bf311e6fefbfff10b45b4a96dac6e4819662246cd11c2e1fe6088"
+      sha256 "527f6f2769160db5de0cd40a712bb908be5ef5d6d82f7f7d4e037f360bf4ea8c"
     end
   end
 
   on_linux do
     on_intel do
       url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.9/gravv_0.1.9_linux_amd64.tar.gz"
-      sha256 "c3d5740c48055b8c333561ca43fb710b57d2469fb5df92df33647043ff98240a"
+      sha256 "b71945485aae18a5e231565e0abd89677a839b5ac572b93cb0a583e57b512d91"
     end
     on_arm do
       url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.9/gravv_0.1.9_linux_arm64.tar.gz"
-      sha256 "c59c451d5dffe47fac7d968ffad7483c87fd377f501528631a32f233849473e5"
+      sha256 "c790e831f0bc40191846ad081b5788b965dd27c9311d3f862e69f336c3037c75"
     end
   end
 
