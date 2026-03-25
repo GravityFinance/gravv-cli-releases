d69a425f061c1d93c4f423d025f39f90e67ab256
Oluwatobi Bello <bellotobiloba01@gmail.com>
2026-03-25 09:39:37 +0100

    chore: update formula to v0.1.8

--- Formula/gravv.rb
+++ Formula/gravv.rb
@@ -6,28 +6,28 @@
 class Gravv < Formula
   desc "CLI/TUI tool for the Gravv financial payments platform"
   homepage "https://github.com/GravityFinance/gravv-cli-releases"
-  version "0.1.7"
+  version "0.1.8"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.7/gravv_0.1.7_darwin_amd64.tar.gz"
-      sha256 "74a4c6872d9cd684dc24d7242bff808193cf071c9ef95b85bdfc2071ac1405e8"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.8/gravv_0.1.8_darwin_amd64.tar.gz"
+      sha256 "6127f1b870e0f201b64c08ad20fc87f4df1e4339b4b2669a2e86bd684dba7fb1"
     end
     on_arm do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.7/gravv_0.1.7_darwin_arm64.tar.gz"
-      sha256 "0dbdc163bd26cf3144ed5246d18b71f081ab10db0da38b2563b8ed19dad73611"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.8/gravv_0.1.8_darwin_arm64.tar.gz"
+      sha256 "5b11c178a0ccd3a2eeb7072abceb40581b061a4fb2539ecdb5e7dcff4ac563a7"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.7/gravv_0.1.7_linux_amd64.tar.gz"
-      sha256 "128a4ae353c4796643cc30d65c3d3dc96d3facf311b60dd3a908913327c45aff"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.8/gravv_0.1.8_linux_amd64.tar.gz"
+      sha256 "928a1cbba224ffcf0f024fdf06ea3dfd8ebc6a06b9b59efa2976dcc06d65bea1"
     end
     on_arm do
-      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.7/gravv_0.1.7_linux_arm64.tar.gz"
-      sha256 "c4a915fbde786e08f965c56a6f6318c995cf0162fcd26c35e1abc12397ee707b"
+      url "https://gravv-cli.s3.us-east-1.amazonaws.com/releases/v0.1.8/gravv_0.1.8_linux_arm64.tar.gz"
+      sha256 "b041a55f575a40e2b7ab5b45140f660e093a864c535130e0fa927b9f6801f510"
     end
   end
 
